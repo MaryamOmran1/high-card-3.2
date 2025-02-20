ac38bee66c7646edf9888825535f209315962505
Maryam Omran <maryam.omran03@gmail.com>
2025-02-21 00:59:42 +0300

    high-card-3.2

---
+++ PlayingCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const PlayingCard = ({ name, suit }) => (
+  <div className="card">
+    <h3>{name}</h3>
+    <p>{suit}</p>
+  </div>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,4 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const currCardElems = currCards.map((card) => <PlayingCard key={'${card.name}${card.suit}'} {...card} />);
